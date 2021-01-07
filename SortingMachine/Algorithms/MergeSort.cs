0ea260f05a6f2b807f35a3cfe1e11231167fc20e
john.paez <john.paez@datasolutions.com.co>
2021-01-07 08:34:21 -0500

    Add partial MergeSort

---
+++ SortingMachine/Algorithms/MergeSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingMachine.Algorithms
+{
+    public class MergeSort : AlgorithmBase
+    {
+        public override string Name => "Merge Sort";
+
+        protected internal override void Perform()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
